accb6cbec8679f07c14202d497a5590dc3ef5288
Павло Морозов <79971428+leonidkstr@users.noreply.github.com>
2021-12-16 23:52:11 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#last 2 srings in input file should be commented like in example
+STRFILE=$(wc -l < input.txt)
+for (( counter = 1; counter <$STRFILE; counter++ ))
+do
+IPVAR=$(head -n $counter input.txt | tail -n 1)
+echo $(ping -c 1 $IPVAR | head -n 5 | tail -n 3) >> output.txt
+done
