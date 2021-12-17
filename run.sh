5a9587f1a2d080458a704e94aae9df4a4384fbaa
Павло Морозов <79971428+leonidkstr@users.noreply.github.com>
2021-12-17 08:23:23 +0000

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ STRFILE=$(wc -l < input.txt)
 for (( counter = 1; counter <$STRFILE; counter++ ))
 do
 IPVAR=$(head -n $counter input.txt | tail -n 1)
+HOSTVAR=$(nslookup $IPVAR | head -n 2 | tail -n 1)
+echo $HOSTVAR >> output.txt
 echo $(ping -c 1 $IPVAR | head -n 5 | tail -n 3) >> output.txt
 done
